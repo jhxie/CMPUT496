57bafecb3349c09f9f40ba59fc8dce5c80da4b6d
Jiahui Xie <xtom32@gmail.com>
2016-02-03 02:11:34 -0700

    Changed all iperf tests to the client side and added the latency and loss rate tests

--- PerformanceAnalysis/SingleSwitchTopo.py
+++ PerformanceAnalysis/SingleSwitchTopo.py
@@ -10,11 +10,21 @@ from mininet.topo import Topo
 class SingleSwitchTopo(Topo):
     """
     Single switch connected to n hosts.
+    Please see the caveat in the overriden 'build' function documentation.
     """
     _buildOption = {"default": 1, "delay": 0, "loss": 0}
 
     def build(self, n=2):
+        """
+        Overrides the default 'build' member function(or method) rather
+        than '__init__' as recommended in the mininet documentation since
+        version 2.2.0 to build a network with 2 hosts.
+        Please DO NOT change the default parameter to other number of hosts!
+        Otherwise you may encounter inconsistencies in testing the topology
+        because only the FIRST LINK is set with appropriate attributes.
+        """
         switch = self.addSwitch("s1")
+        buildOption = SingleSwitchTopo.getBuildOption()
         # by default mininet's host index starts from 1,
         # so we follow its convention
         for host_idx in range(n):
@@ -22,21 +32,47 @@ class SingleSwitchTopo(Topo):
             # is left as default (unspecified)
             host = self.addHost("h%s" % (host_idx + 1))
             # link is set to be 1Gbps with no delay/loss
-            if 0 != SingleSwitchTopo.getBuildOption()["default"]:
+            if 0 != buildOption["default"]:
                 self.addLink(host, switch, bw=1000)
-            elif 0 != SingleSwitchTopo.getBuildOption()["delay"]:
-                delay_val = SingleSwitchTopo.getBuildOption()["delay"]
+                continue
+            # -------------------------- CAVEAT -------------------------------
+            # Only the FIRST LINK is set with the specifed attribute
+            # for both 'delay' and 'loss', so this topology should NEVER be
+            # used with MORE THAN 2 HOSTS!
+            # Otherwise you MUST use the FIRST HOST to avoid inconsistencies
+            # in various test cases.
+            # -------------------------- CAVEAT -------------------------------
+            elif 0 == host_idx and 0 != buildOption["delay"]:
+                delay_val = buildOption["delay"]
                 self.addLink(host, switch, bw=1000, delay=str(delay_val)+"ms")
-            elif 0 != SingleSwitchTopo.getBuildOption()["loss"]:
-                loss_val = SingleSwitchTopo.getBuildOption()["loss"]
+                continue
+            elif 0 == host_idx and 0 != buildOption["loss"]:
+                loss_val = buildOption["loss"]
                 self.addLink(host, switch, bw=1000, loss=loss_val)
+                continue
+            # remember to attach the 2nd host
+            self.addLink(host, switch, bw=1000)
 
     @classmethod
     def getBuildOption(topo):
+        """
+        Returns the class-wide (pseudo-static) attribute build option.
+        """
         return topo._buildOption
 
     @classmethod
     def setBuildOption(topo, option, arg=1):
+        """
+        Sets the class-wide (pseudo-static) attribute build option.
+        The option itself is an embedded dictionary, and the valid
+        keys are 'default', 'delay', and 'loss'.
+        """
+        # Why potentially over-complicates the codebase with all those
+        # fancy classmethod decorators?
+        # Well, turns out specifying options like 'bw', 'delay' in the
+        # overriden 'build' member function directly run into all sorts of
+        # problems that the author have no idea why and is not able to fix,
+        # so here it is.
         validOptions = SingleSwitchTopo.getBuildOption().keys()
         if option not in validOptions:
             raise ValueError("option must be one of " +
--- PerformanceAnalysis/perfTest.py
+++ PerformanceAnalysis/perfTest.py
@@ -3,6 +3,8 @@
 from __future__ import print_function  # use python3 print function instead
 
 import matplotlib.pyplot as plt
+import os
+import sys
 
 from mininet.net import Mininet
 from mininet.link import TCLink
@@ -12,10 +14,25 @@ from mininet.log import setLogLevel
 from SingleSwitchTopo import SingleSwitchTopo
 
 
-def perfTest():
+reportNames = ["IperfClientFileSizeReport",
+               "IperfClientLatencyReport",
+               "IperfClientLossReport"]
+
+
+def perfTestFileSize(rangeMin=1, rangeMax=11):
     """
-    Create network and run performance test  with 1 switch and 2 hosts.
+    Create network and run performance test using file size as a parameter
+    (i.e. no message loss or latency) with 1 switch and 2 hosts topology.
+    The 2 optional parameters stand for the lower and upper bound exponent
+    of file size with 2 as the base.
+    Example
+    perfTestFileSize(1,11) would lead to file size range from 2MB up to 1024MB.
     """
+    for argument in (rangeMin, rangeMax):
+        if not isinstance(argument, int) or 0 > argument:
+            raise ValueError("argument must be non-negative integers")
+
+    print("!! Performing Network Performance Test Using File Size !!")
     topo = SingleSwitchTopo(n=2)
     net = Mininet(topo, link=TCLink)
     net.start()
@@ -34,49 +51,177 @@ def perfTest():
     # We choose to run 'iperf' as a command instead in order to pass
     # custom command line flags to it
     # net.iperf(hosts=(h1, h2), l4Type="TCP")
-    h1.cmd("iperf -f m -s > /tmp/IperfServerReport &")
+    h1.cmd("iperf -f m -s > /dev/null 2>&1 &")
 
     # File to be transferred up to 2 ^ 10
-    for mb in range(1, 11):
-        print("File Size -------- [{}MB]".format(str(2 ** mb)))
+    for mb in range(rangeMin, rangeMax):
+        print("File Size -------- [{} MB]".format(str(2 ** mb)))
         h2.cmd("dd if=/dev/zero of={}M bs=1M count={}"
                .format(str(2 ** mb), str(2 ** mb)))
-        h2.cmd("iperf -f m -c {} -F {}M  > /dev/null 2>&1"
+        h2.cmd("iperf -f m -c {} -F {}M  >> /tmp/IperfClientFileSizeReport"
                .format(h1.IP(), str(2 ** mb)))
         h2.cmd("rm -f {}M".format(str(2 ** mb)))
 
     h1.cmd("kill %")
     h2.cmd("kill %")
     net.stop()
+    print("!! Final iperf Report on File Size from Client Side !!")
 
-    print("!! Final iperf Report from Server Side !!")
-    with open("/tmp/IperfServerReport") as report:
-        print(report.read())
+
+def perfTestLatency(rangeMin=1, rangeMax=182, rangeStep=20):
+    """
+    Create network and run performance test using latency as a parameter
+    (i.e. no message loss or variation in file size, holding time constant,
+    which is set to be 10 seconds by default for iperf version 2) with 1
+    switch and 2 hosts topology.
+    Here the parameters stands for the latency range in milisecond.
+    """
+    for argument in (rangeMin, rangeMax, rangeStep):
+        if not isinstance(argument, int) or 0 > argument:
+            raise ValueError("argument must be non-negative integers")
+
+    print("!! Performing Network Performance Test Using Latency !!")
+    # From mininet's documentation as well as in-source comment it is not clear
+    # how to set the RTT directly, so delay is used here.
+    # In other words, the actual RTT = 2 * delay in a single switch network
+    # since ONLY THE FIRST LINK in a single switch network is set to 'delay'.
+    # Also, how to change the delay of established link is unknown to the
+    # author so network topology is set up and tear down repeatedly
+    # which may not be an elegant or efficient way of implementing it
+    # The actual sequence is like [1, 21, 41, ... 141, 161] where n = 9
+    for delay in range(rangeMin, rangeMax, rangeStep):
+        SingleSwitchTopo.setBuildOption("delay", delay)
+        topo = SingleSwitchTopo(n=2)
+        net = Mininet(topo, link=TCLink)
+        net.start()
+        print("!! Dumping host connections !!")
+        dumpNodeConnections(net.hosts)
+        print("!! Testing network connectivity !!")
+        net.pingAll()
+        h1, h2 = net.getNodeByName("h1", "h2")
+        print("!! Testing bandwidth between h1 ({}) and h2 ({}) !!"
+              .format(h1.IP(), h2.IP()))
+        # since both hosts would be tear down at each iteration
+        # we have to be careful and remember to use 'append' shell redirection
+        h1.cmd("iperf -f m -s > /dev/null 2>&1 &")
+        print("Latency -------- [{} ms]".format(str(delay)))
+        h2.cmd("iperf -f m -c {} >> /tmp/IperfClientLatencyReport"
+               .format(h1.IP()))
+        h2.cmd("kill %")
+        h1.cmd("kill %")
+        net.stop()
+
+    print("!! Final iperf Report on Latency from Client Side !!")
+
+
+def perfTestLoss(rangeMin=1, rangeMax=6):
+    """
+    Create network and run performance test using message loss as a parameter
+    (i.e. no variation in file size or latency, fix transfer time period as 10
+    seconds, as mentioned previously) with 1 switch and 2 hosts topology.
+    The argument stands for the PERCENTAGE for data loss on the FIRST LINK.
+    """
+    for argument in (rangeMin, rangeMax):
+        if not isinstance(argument, int) or 0 > argument:
+            raise ValueError("argument must be non-negative integers")
+
+    print("!! Performing Network Performance Test Using Message Loss Rate !!")
+    # in mininet the loss rate can only be expressed in decimal,
+    # no fractions are allowed; i.e. 0.1% data loss rate is not possible
+    # here the loss rate range between [1, 10]
+    for loss_rate in range(rangeMin, rangeMax):
+        SingleSwitchTopo.setBuildOption("loss", loss_rate)
+        topo = SingleSwitchTopo(n=2)
+        net = Mininet(topo, link=TCLink)
+        net.start()
+        print("!! Dumping host connections !!")
+        dumpNodeConnections(net.hosts)
+        print("!! Testing network connectivity !!")
+        net.pingAll()
+        h1, h2 = net.getNodeByName("h1", "h2")
+        print("!! Testing bandwidth between h1 ({}) and h2 ({}) !!"
+              .format(h1.IP(), h2.IP()))
+        h1.cmd("iperf -f m -s > /dev/null 2>&1 &")
+        print("Loss Rate -------- [{} %]".format(str(loss_rate)))
+        h2.cmd("iperf -f m -c {} >> /tmp/IperfClientLossReport"
+               .format(h1.IP()))
+        h2.cmd("kill %")
+        h1.cmd("kill %")
+        net.stop()
+
+    print("!! Final iperf Report on Loss Rate from Client Side !!")
+
+
+def perfTestAllMetrics():
+    """
+    Clean all the past generated iperf reports and run all tests.
+    Remember to execute the script with proper permission to make the 'rm'
+    command actually work.
+    """
+    os.system("cd /tmp/ && rm -f " + " ".join(reportNames))
+    perfTestFileSize()
+    perfTestLatency()
+    perfTestLoss()
+
+
+def perfParseResult():
+    fileSizeBandwidthList = []
+    latencyBandwidthList = []
+    lossBandwidthList = []
+    for reportName in reportNames:
+        with open("/tmp/" + reportName, "r") as report:
+            for line in report:
+                # The built-in function all() described from official doc
+                # "
+                # Return True if all elements of the iterable are true
+                # (or if the iterable is empty)
+                # "
+                # As a result, the following if conditional is only executed
+                # when both keywords are in the line since the generator
+                # expression returns an iterable of True/False values
+                if all(keyword in line for keyword in ("MBytes", "Mbits/sec")):
+                    measuredBandwidth = float(line.split(" ")[-2])
+                    if "IperfClientFileSizeReport" == reportName:
+                        fileSizeBandwidthList.append(measuredBandwidth)
+                    elif "IperfClientLatencyReport" == reportName:
+                        latencyBandwidthList.append(measuredBandwidth)
+                    elif "IperfClientLossReport" == reportName:
+                        lossBandwidthList.append(measuredBandwidth)
+    return fileSizeBandwidthList, latencyBandwidthList, lossBandwidthList
 
 
 def perfPlotResult():
-    bandwidthList = []
-    with open("/tmp/IperfServerReport", "r") as report:
-        for line in report:
-            # The built-in function all() described from official python doc,
-            # "
-            # Return True if all elements of the iterable are true (or if the
-            # iterable is empty)
-            # "
-            # As a result, the following if conditional is only executed when
-            # both keywords are in the line since the generator expression
-            # returns an iterable of True/False values
-            if all(keyword in line for keyword in ("MBytes", "Mbits/sec")):
-                bandwidthList.append(int(line.split(" ")[-2]))
+    """
+    Plot the performance graph using the report generated by
+    'perfTestFileSize' 'perfTestLatency' 'perfTestLoss'.
+    """
 
-    plt.plot([2 ** i for i in range(1, 11)], bandwidthList, "ro")
-    plt.axis([0, 2 ** 10, 0, 2 ** 10])
-    plt.xlabel("File Size (MB)")
-    plt.ylabel("Bandwidth (Mbps)")
+    fileSizeBandwidth, latencyBandwidth, lossBandwidth = perfParseResult()
+
+    _, axisArray = plt.subplots(3, sharey=True)
+
+    axisArray[0].plot([2 ** i for i in range(1, 11)], fileSizeBandwidth, "ro-")
+    axisArray[0].axis([0, 2 ** 10, 0, 2 ** 10])
+    axisArray[0].set_title("Performance Analysis Using Iperf/Mininet")
+    axisArray[0].set_xlabel("File Size (MB)")
+    axisArray[0].set_ylabel("Bandwidth (Mbps)")
+    axisArray[0].grid(True)
+
+    axisArray[1].plot([i for i in range(1, 182, 20)], latencyBandwidth, "bs-")
+    axisArray[1].axis([0, 162, 0, 2 ** 10])
+    axisArray[1].set_xlabel("Latency (Miliseconds)")
+    axisArray[1].set_ylabel("Bandwidth (Mbps)")
+    axisArray[1].grid(True)
+
+    axisArray[2].plot([i for i in range(1, 6)], lossBandwidth, "g^-")
+    axisArray[2].axis([0, 11, 0, 2 ** 10])
+    axisArray[2].set_xlabel("Loss Rate (%)")
+    axisArray[2].set_ylabel("Bandwidth (Mbps)")
+    axisArray[2].grid(True)
+
     plt.show()
 
 if __name__ == "__main__":
-    import sys
     # Tell mininet to print useful information
     setLogLevel("info")
     perfTestFlags = ["-t", "--test"]
@@ -84,11 +229,11 @@ if __name__ == "__main__":
     perfAllFlags = ["-a", "--all"]
 
     if any(cmdFlag in sys.argv for cmdFlag in perfTestFlags):
-        perfTest()
+        perfTestAllMetrics()
     elif any(cmdFlag in sys.argv for cmdFlag in perfPlotResultFlags):
         perfPlotResult()
     elif any(cmdFlag in sys.argv for cmdFlag in perfAllFlags):
-        perfTest()
+        perfTestAllMetrics()
         perfPlotResult()
     else:
         print("[Usage]")
